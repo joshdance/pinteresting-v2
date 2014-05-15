7df603ced90f83290bf011810b0f553793f687fd
Joshua Dance <joshua.dance@gmail.com>
2014-05-14 19:49:56 -0600

    added devise gem

--- Gemfile
+++ Gemfile
@@ -17,6 +17,8 @@ gem 'jbuilder', '~> 1.2'
 
 gem 'bootstrap-sass'
 
+gem 'devise', '~> 3.1.0.rc2'
+
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #Note to set this to your actual host - DONE
+  config.action_mailer.default_url_options = { :host => 'dancej-pinteresting.herokuapp.com/' }
 end
