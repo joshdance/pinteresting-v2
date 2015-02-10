013d406dc2626c8e6dfbff4b002a8110a4988f91
Joshua Dance <joshua.dance@gmail.com>
2015-02-10 15:02:04 -0700

    added pins to users, users own pins.

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,8 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  #before filter and before action are same thing. 
 
   def index
     @pins = Pin.all
@@ -10,14 +13,15 @@ class PinsController < ApplicationController
   end
 
   def new
-    @pin = Pin.new
+    @pin = current_user.pins.build 
+    #current_user just gives you the @User, building a pin via that way sets the user id. 
   end
 
   def edit
   end
 
   def create
-    @pin = Pin.new(pin_params)
+    @pin = current_user.pins.build(pin_params)
     if @pin.save
       redirect_to @pin, notice: 'Pin was successfully created.'
     else
@@ -42,6 +46,11 @@ class PinsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_pin
       @pin = Pin.find(params[:id])
+    end
+
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "You can't edit that pin. Sorry." if @pin.nil?
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
